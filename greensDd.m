b96770949cadd1fc27586541be4880f2832a29f9
qyxiao <qx344@nyu.edu>
2017-12-06 23:14:38 -0500

    test code
    the result not correct; matrix close to singular?

--- greensDd.m
+++ greensDd.m
@@ -1,9 +1,20 @@
-function  output = greensDd( x, y )
+function  output = greensDd( X, Y, i, z1, z2 )
 
 % directional derivative of green function
-% TODO
 
+% assuming boundary is circle centered at origin with radius 1
+% thus give normal direction = y
+if(i==0)
+output = ( (X-z1).*X + (Y-z2).*Y  )./(2*pi* ((X-z1).^2+(Y-z2).^2) );
+    
+else
+x0 = X(i); y0 = Y(i);
+X(i)=X(i)+1; Y(i)=Y(i)+1; % avoid singularity value
 
+%output = sum((x-y).*y)/(2*pi*sum((x-y).^2));
+output =   ( (X-x0).*X + (Y-y0).*Y  )./(2*pi* ((X-x0).^2+(Y-y0).^2) );
+output(i) = 0;
+end
 
 end
 
--- simpletry.m
+++ simpletry.m
@@ -1,17 +1,42 @@
 
+%clear all; clc
 
-N = 20; length = 2*pi;
+N = 60; length = 2*pi;
 
-theta = linspace(0,length,N);
-
+theta_all = linspace(0,length,N+1);
+theta = theta_all(1:N);
 [X,Y] = sphere(1, theta);
 
+weight1 = [1.825748 -1.325748];
+weight2 = [4.967362 -1.6205015 2.585153 -2.222599 9.930104 -1.817995];
+weight3 = [7.832432 -4.5651617 1.452168 -2.901348 3.870862 -3.523821 2.172421 -8.707796 2.053584 -2.166984];
+
+h = length/N;
+
+sample_acc = 2;
+sample_weight = ones(1,N); 
+sample_weight(1) = 0;
+sample_weight(2:2+sample_acc-1) = sample_weight(2:2+sample_acc-1) + weight1;
+sample_weight(N-sample_acc+1:N) = sample_weight(N-sample_acc+1:N) + fliplr(weight1);
+sample_weight = h*sample_weight;
 
 
+weight_matrix = zeros(N,N);
+for i=1:N
+   
+   greenDD = greensDd(X, Y, i, 0, 0);
+   weight_matrix(i,:) = circshift(sample_weight,i-1).*greenDD;
+end
+LHS =   weight_matrix + eye(N,N)/2;
+density = 2*LHS\ones(N,1);
+%density = 2*weight_matrix\ones(N,1);
 
+a = linspace(-0.9,0.9,30); b = [];
+for i = a
+b = [b sum(greensDd(X,Y,0,i,i).*density')*h];
+end
+figure(1);hold on; plot(a,b);title(['N=',num2str(N)]);
 
+%value = sum(greensDd(X,Y,0,0,0).*density')*h;
 
-
-
-
-figure(1); plot(X,Y,'r*')
+%figure(1); plot(X,Y,'r*')
