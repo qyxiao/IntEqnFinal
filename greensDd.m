57476b2144d0d8ae23e4af7f85b6377748535dc4
qyxiao <qx344@nyu.edu>
2017-12-10 19:32:46 -0500

    test 2 passed

--- greensDd.m
+++ greensDd.m
@@ -10,14 +10,12 @@ mag = sqrt(X.^2+Y.^2);
 if(i==0)
 output = -( (z1-X).*(X)./mag + (z2-Y).*Y./mag  )./(2*pi* ((X-z1).^2+(Y-z2).^2) );
     
-% else
-% x0 = X(i); y0 = Y(i);
-% X(i)=X(i)+1; Y(i)=Y(i)+1; % avoid singularity value
-% 
-% %output = sum((x-y).*y)/(2*pi*sum((x-y).^2));
-% %output =   ( (x0-X).*X + (y0-Y).*Y  )./(2*pi* ((X-x0).^2+(Y-y0).^2) );
-% output =   ( (x0-X).*(X) + (y0-Y).*Y  )./(2*pi* ((X-x0).^2+(Y-y0).^2) );
-% output(i) = 0;
+else
+x0 = X(i); y0 = Y(i);
+X(i)=X(i)+1; Y(i)=Y(i)+1; % avoid singularity value
+ 
+output = -( (x0-X).*(X)./mag + (y0-Y).*Y./mag  )./(2*pi* ((X-x0).^2+(Y-y0).^2) );
+output(i) = 0;
 end
 
 end
