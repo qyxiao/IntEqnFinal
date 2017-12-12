5e74b049f4f0d3fbc1c0bd938bfe87556884e350
qyxiao <qx344@nyu.edu>
2017-12-11 20:47:00 -0500

    test 3

--- simpletry.m
+++ simpletry.m
@@ -1,11 +1,12 @@
 
-%clear all; clc
+clear all; clc
 
-N = 60; length = 2*pi;
+N = 180; length = 2*pi; 
 
 theta_all = linspace(0,length,N+1);
 theta = theta_all(1:N);
-[X,Y] = sphere(1, theta);
+[X,Y] = sphere(1, theta); 
+p0_x = 20; p0_y = 0;
 
 weight1 = [1.825748 -1.325748];
 weight2 = [4.967362 -1.6205015 2.585153 -2.222599 9.930104 -1.817995];
@@ -27,15 +28,18 @@ for i=1:N
    greenDD = greensDd(X, Y, i, 0, 0);
    weight_matrix(i,:) = circshift(sample_weight,i-1).*greenDD;
 end
-LHS =   weight_matrix + eye(N,N)/2;
-density = 2*LHS\ones(N,1);
+LHS =  -eye(N,N)/2 - weight_matrix ;
+density = LHS\greens(X,Y,0,p0_x,p0_y)';
 %density = 2*weight_matrix\ones(N,1);
 
 a = linspace(-0.9,0.9,30); b = [];
 for i = a
-b = [b sum(greensDd(X,Y,0,i,i).*density')*h];
+b = [b -sum(greensDd(X,Y,0,i,i).*density')*h];
 end
-figure(1);hold on; plot(a,b);title(['N=',num2str(N)]);
+figure(2);hold on; plot(a,b);title(['N=',num2str(N)]);
+
+expected = -1/(2*pi)*log(sqrt((a-p0_x).^2+(a-p0_y).^2));
+plot(a,expected);hold off;legend('solver','expected') 
 
 %value = sum(greensDd(X,Y,0,0,0).*density')*h;
 
